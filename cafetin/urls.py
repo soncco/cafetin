21655b842730dd10f443cbf7b04c5567f7967b9c
brau <soncco@gmail.com>
2013-08-12 19:07:06 -0500

    Urls de mozo y cocina

--- cafetin/urls.py
+++ cafetin/urls.py
@@ -10,11 +10,14 @@ urlpatterns = patterns('cafetin.views',
 
   url(r'^cliente/(?P<q>.*)/$', 'busqueda_clientes', name = 'clientes_json'),
 
+  url(r'^plato/(?P<local>.*)/(?P<q>.*)/$', 'busqueda_platos_local', name = 'platos_local_json'),
   url(r'^plato/(?P<q>.*)/$', 'busqueda_platos', name = 'platos_json'),
+  
 
   url(r'^carta/json$', 'carta', name = 'carta_json'),
   url(r'^pedido/add$', 'add_pedido', name = 'agrega_pedido'),
   url(r'^pedido/json$', 'json_pedidos', name = 'pedidos_json'),
+  url(r'^pedido/json/mozo/(?P<mozo>.*)/$', 'json_pedidos_mozo', name = 'pedidos_mozo_json'),
   url(r'^pedido/remove$', 'remove_pedido', name = 'quita_pedido'),
   url(r'^pedido/atender$', 'atender_pedido', name = 'atender_pedido'),
   url(r'^pedido/printed$', 'pedido_printed',   name = 'pedido_printed'),
