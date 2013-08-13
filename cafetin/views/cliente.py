dcdfb8a160c53249ca0a2983051c227b85757c06
brau <soncco@gmail.com>
2013-08-13 01:36:13 -0500

    Clientes por local

--- cafetin/views/cliente.py
+++ cafetin/views/cliente.py
@@ -2,10 +2,10 @@
 
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-from cafetin.models import Cliente
+from cafetin.models import Cliente, Local
 import json
 
-# Locales.
+# Clientes.
 def busqueda_clientes(request, q):
   clientes = Cliente.objects.filter(nombres__icontains = q) | Cliente.objects.filter(apellidos__icontains = q)
   callback = request.GET.get('callback') #Jsonp callback function.
@@ -16,5 +16,22 @@ def busqueda_clientes(request, q):
       'nombres': cliente.nombres,
       'apellidos': cliente.apellidos,
     })
+
+  return HttpResponse(callback + '(' + json.dumps(response) + ')', mimetype = "application/json")
+
+def busqueda_clientes_local(request, local, q):
+  local = Local.objects.get(id = local)
+
+  clientes = Cliente.objects.filter(nombres__icontains = q, activo = True) | Cliente.objects.filter(apellidos__icontains = q, activo = True)
+  callback = request.GET.get('callback') #Jsonp callback function.
+  response = []
+  for cliente in clientes:
+    print cliente.hospedado_en.pertenece_a.id
+    if cliente.hospedado_en.pertenece_a == local:
+      response.append({
+        'id': cliente.id,
+        'nombres': cliente.nombres,
+        'apellidos': cliente.apellidos,
+      })
 
   return HttpResponse(callback + '(' + json.dumps(response) + ')', mimetype = "application/json")
